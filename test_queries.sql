a0ec899f7da4e7612c4de70cd397f1ef18a7b370
Morris L <morrislli@outlook>
2026-03-05 08:17:02 -0800

    fixed incorrect 'name' references'

--- test_queries.sql
+++ test_queries.sql
@@ -35,80 +35,81 @@ BEGIN
 END$$;
 
 -- Check that null term names can't be inserted
-DO$$
+DO $$
 BEGIN
     INSERT INTO terms
     VALUES (202540, NULL, '2026-01-01', '2026-04-01');
 
-    EXCEPTION WHEN required_attribute_violation THEN
+    EXCEPTION WHEN not_null_violation THEN
         RAISE NOTICE 'non-null term title correctly rejected';
     WHEN OTHERS THEN
         RAISE EXCEPTION 'Null term title incorrectly accepted';
 END$$;
 
 -- Checks for sets:
-SELECT COUNT(*) FROM sets;
--- expect 6
+DO $$
+DECLARE
+    set_count int;
+BEGIN
+    SELECT COUNT(*) INTO set_count FROM sets;
+    ASSERT set_count = 6, 'Expected 6 items in sets, got ' || set_count;
+END$$;
 
-INSERT INTO sets
-VALUES()
-
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT set_code)
 from sets;
-
 
 -- Checks for courses:
 SELECT COUNT(*) FROM courses;
 -- expect 1
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT title)
 from courses;
 
 -- Checks for sections:
 SELECT COUNT(*) FROM sections;
 -- expect 6
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT section_code)
 from sections;
 
 -- Checks for students:
 SELECT COUNT(*) FROM students;
 -- expect 18
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT student_id)
 from students;
 
 -- Checks for lab_assignments:
 SELECT COUNT(*) FROM lab_assignments;
 -- expect 8
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT assignment_id)
 from lab_assignments;
 
 -- Checks for lab_events:
 SELECT COUNT(*) FROM lab_events;
 -- expect 18
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT event_id)
 from lab_events;
 
 -- Checks for terms:
 SELECT COUNT(*) FROM terms;
 -- expect 3
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT term_code)
 from terms;
 
 -- Checks for progress:
 SELECT COUNT(*) FROM progress;
 -- expect 37
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT progress_id)
 from progress;
 
 -- Checks for progress_change_log:
 SELECT COUNT(*) FROM progress_change_log;
 -- expect 3
 
-SELECT COUNT(DISTINCT name)
+SELECT COUNT(DISTINCT change_id)
 from progress_change_log;
